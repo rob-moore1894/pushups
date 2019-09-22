785e86d95b80718f450f763f8ceeb5ccbb790fa0
rob-moore1894 <moore.re@gmail.com>
2019-09-21 23:58:50 -0500

    Updated Grand Canyon for a Grinch response

--- js/grandCanyon.js
+++ js/grandCanyon.js
@@ -10,11 +10,13 @@ function echo() {
     for (var i = 1; i <= numberOfEchoes; i++){
         var phrase = document.getElementById("echoPhrase").value;
         var newPhrase = phrase + "<br />";
-        if (numberOfEchoes > 1) {
-            $("#response").html("\"" + phrase + "\" should repeat " + numberOfEchoes + " times!");
-        } else {
-            $("#response").html("\"" + phrase + "\" should repeat 1 time!");
+        if (phrase === ""){
+            $("#response").html("I'm going to need you to say something for this to work");
+        } else if (phrase.toLowerCase() === "i'm an idiot" || phrase.toLowerCase() === "i am an idiot"){
+            newPhrase = "YOU'RE AN IDIOT! <br />";
+            $("#response").html("YOU'RE AN IDIOT!")
         }
+
         $('#sayAgain').html(newPhrase.repeat(numberOfEchoes));
 
     }
